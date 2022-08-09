c3bcc9be3089497177acc5080250d942f2fde08f
harshita26 <harshitakhandelwal925@gmail.com>
2022-08-09 20:24:34 +0530

    Updates

---
+++ precache-manifest.aa556efa329e07e7ffdb818014fe0d5f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ceb43361c56583ba5e8a52ee70478ee7",
+    "url": "/Todo-list-React/index.html"
+  },
+  {
+    "revision": "1ab8e7f694496ff2a6eb",
+    "url": "/Todo-list-React/static/js/2.ecaa24d3.chunk.js"
+  },
+  {
+    "revision": "3a6024aa35cf9a592230",
+    "url": "/Todo-list-React/static/js/main.99cae6ad.chunk.js"
+  },
+  {
+    "revision": "0601db3beec10ddc94fb",
+    "url": "/Todo-list-React/static/js/runtime-main.a4df7d96.js"
+  }
+]);
