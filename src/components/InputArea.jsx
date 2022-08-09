25f213a4149a73561714d97e8379bd643b417fd5
harshita26 <harshitakhandelwal925@gmail.com>
2022-08-09 19:19:09 +0530

    Initial commit

---
+++ src/components/InputArea.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function InputArea(props) {
+  return (
+    <div className="form">
+      <input
+        onChange={(event) => {
+          props.handleChange(event);
+        }}
+        type="text"
+        value={props.inputText}
+      />
+      <button onClick={props.addItem}>
+        <span>Add</span>
+      </button>
+    </div>
+  );
+}
+
+export default InputArea;
